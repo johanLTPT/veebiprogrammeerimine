f9bed5c09b626f5b80238c8c815376ef5443be9a
Johan Leoste <johanLTPT@userrs.noreply.github.com>
2020-09-21 22:38:32 +0300

    Kodutöö nr3

---
+++ Kodu3/home.php
@@ -0,0 +1,15 @@
+<?php
+  //var_dump($_POST);
+  require("../../../config.php");
+  require("picshow.php");
+  require("header.php");
+?>
+  <?php require("semestershow.php"); ?>
+  <?php echo $imghtml; ?>
+  <hr>
+  <p>Lingid</p>
+  <a href="ideainput.php">Sisesta ideed</a>
+  <a href="ideashow.php">Näita ideid</a>
+ 
+</body>
+</html>
