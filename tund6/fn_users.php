3a38c98f4d40d6ae0eb5c4e76d31d0d97596b869
Johan Leoste <johanLTPT@userrs.noreply.github.com>
2020-10-06 11:35:11 +0300

    tund6

---
+++ tund6/fn_users.php
@@ -0,0 +1,134 @@
+<?php
+$database = "if20_johan_le_1";
+function signup($firstname, $lastname, $email, $gender, $birthdate, $password)
+{
+    $notice = null;
+    $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+    $stmt = $conn->prepare("INSERT INTO vpusers (firstname, lastname, birthdate, gender, email, password) VALUES(?,?,?,?,?,?)");
+    echo $conn->error;
+    //Krüpteerime passwordi
+    $options = ["cost" => 12, "salt" => substr(sha1(rand()), 0, 22)];
+    $pwdhash = password_hash($password, PASSWORD_BCRYPT, $options);
+    //edasi
+    $stmt->bind_param("sssiss", $firstname, $lastname, $birthdate, $gender, $email, $pwdhash);
+    if ($stmt->execute()) {
+        $notice = "ok";
+    } else {
+        $notice = $stmt->error;
+    }
+    $stmt->close();
+    $conn->close();
+    return $notice;
+}
+
+function signin($email, $password)
+{
+    $notice = null;
+    $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+    $stmt = $conn->prepare("SELECT password FROM vpusers WHERE email = ?");
+    echo $conn->error;
+    $stmt->bind_param("s", $email);
+    $stmt->bind_result($passwordfromdb);
+    if ($stmt->execute()) {
+        //tehnilisel korras
+        if ($stmt->fetch()) {
+            //kasutaja leiti
+            if (password_verify($password, $passwordfromdb)) {
+                //parool õige
+                $stmt->close();
+                //loen kasutaja infot
+                $stmt = $conn->prepare("SELECT vpusers_id,firstname,lastname FROM vpusers WHERE email = ?");
+                echo $conn->error;
+                $stmt->bind_param("s", $email);
+                $stmt->bind_result($idfromdb, $firstnamefromdb, $lastnamefromdb);
+                $stmt->execute();
+                $stmt->fetch();
+                //Salvestame sessioonimuutujad
+                $_SESSION["userid"] = $idfromdb;
+                $_SESSION["userfirstname"] = $firstnamefromdb;
+                $_SESSION["userlastname"] = $lastnamefromdb;
+                //värvid lugeda profiilist, kui olemas
+                if(isset($_SESSION["userbgcolor"])){
+                
+                }else{}
+                $_SESSION["userbgcolor"] = "#FFF";
+                $_SESSION["usertextcolor"] = "#000";
+                }
+                $stmt->close();
+                $conn->close();
+                header("Location: home.php");
+                exit();
+            } else {
+                $notice = "Vale parool";
+            }
+        } else {
+            $notice = "sellist kasutajat (" . $email . ") ei leitud!";
+        }
+    } else {
+        $notice = $stmt->error;
+    }
+    $stmt->close();
+    $conn->close();
+    return $notice;
+}
+
+function storeuserprofile($userdescription, $bgcolorinput, $txtcolorinput)
+{
+    $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+    $stmt = $conn->prepare("SELECT vpuserprofiles_id FROM vpuserprofiles WHERE userid = ?");
+    echo $conn->error;
+    $notice = "";
+    $stmt->bind_param("s", $_SESSION["userid"]);
+    $stmt->bind_result($useridfromdb);
+    if ($stmt->execute()) {
+
+        if ($stmt->fetch()) {
+            //kasutaja on juba tabelis
+            $stmt->close();
+            $stmt = $conn->prepare("UPDATE vpuserprofiles SET description = ?, bgcolor = ?, txtcolor = ? WHERE userid = ?");
+            $stmt->bind_param("sssi", $userdescription, $bgcolorinput, $txtcolorinput, $_SESSION["userid"]);
+        } else {
+            //kasutajat pole tabelis
+            $stmt->close();
+            $stmt = $conn->prepare("INSERT INTO vpuserprofiles (userid, description, bgcolor, txtcolor) VALUES (?,?,?,?)");
+            $stmt->bind_param("isss", $_SESSION["userid"], $userdescription, $bgcolorinput, $txtcolorinput);
+        }
+        if ($stmt->execute()) {
+            $notice = "ok";
+        } else {
+            $notice = $stmt->error;
+        }
+    }
+
+    $stmt->close();
+    $conn->close();
+    return $notice;
+}
+
+function readuserdescription()
+{
+    $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+    $stmt = $conn->prepare("SELECT vpuserprofiles_id FROM vpuserprofiles WHERE userid = ?");
+    $stmt->bind_param("s", $_SESSION["userid"]);
+    $stmt->bind_result($useridfromdb);
+    if ($stmt->execute()) {
+
+        if ($stmt->fetch()) {
+            //kasutaja on juba tabelis
+            $stmt->close();
+            $stmt = $conn->prepare("SELECT description FROM vpuserprofiles WHERE userid = ?");
+            $stmt->bind_param("s", $_SESSION["userid"]);
+            $stmt->bind_result($userdescriptionfromdb);
+            $stmt->execute();
+            $stmt->fetch();
+            
+        } else {
+            $userdescriptionfromdb = "tühi";
+        }
+        
+    }
+
+    $stmt->close();
+    $conn->close();
+    return $userdescriptionfromdb;
+}
---
+++ tund6/ideainput.php
@@ -0,0 +1,40 @@
+<?php
+require("usesession.php");
+  require("../../../config.php");
+  $database = "if20_johan_le_1";
+  
+  if(isset($_POST["ideasubmit"]) and !empty($_POST["ideainput"])){
+	  $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
+	  //sql statementi ettevalmistamine
+	  $stmt = $conn->prepare("INSERT INTO myideas (idea) VALUES(?)");
+	  echo $conn->error;
+	  
+	  //admed, i = integer, d = decimal, s = string
+	  $stmt->bind_param("s", $_POST["ideainput"]);
+	  $stmt->execute();
+	  $stmt->close();
+	  $conn->close();
+  }
+  $username = "";
+  require("header.php");
+?>
+<img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
+<h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"] ?></h1>
+<p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+<p>See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+<hr>
+<form method="POST">
+	<label>Sisesta oma mõtetera </label>
+	<input type="text" name="ideainput" placeholder="kirjuta siia">
+	<input type="submit" name="ideasubmit" value="Pane mõte teele!">
+  </form>
+	<hr>
+	<ul>
+	<li><a href="home.php">Avaleht</a></li>
+	<li><a href="ideashow.php">Näita ideid</a></li>
+	<li><a href="?logout=1" style="color: red;">Logi välja</a></li>
+	</ul>
+	
+
+
+</body>
