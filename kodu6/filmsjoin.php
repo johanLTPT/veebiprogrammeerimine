c609f262917c8d7ad12d1d70f1307157d048260e
Johan Leoste <johanLTPT@userrs.noreply.github.com>
2020-10-13 09:06:53 +0300

    Isikud ja rollid

--- kodu6/filmsjoin.php
+++ kodu6/filmsjoin.php
@@ -4,9 +4,12 @@ require("../../../config.php");
 require("fn_films.php");
 require("header.php");
 
-$notice = "";
+$notice = $notice2 = "";
 $film = "";
+$film2 = "";
 $studio = "";
+$human = "";
+$position = "";
 if (isset($_POST["filmjoinsubmit"])) {
   if (!empty($_POST["filminput"])) {
     $film = $_POST["filminput"];
@@ -19,7 +22,27 @@ if (isset($_POST["filmjoinsubmit"])) {
     $notice = " Vali stuudio!";
   }
   if (!empty($film) and !empty($studio)) {
-    $notice = newrelation($film, $studio);
+    $notice = newfilmandstudio($film, $studio);
+  }
+}
+if (isset($_POST["filmpersonsubmit"])) {
+  if (!empty($_POST["filminput"])) {
+    $film2 = $_POST["filminput"];
+  } else {
+    $notice2 = " Vali film!";
+  }
+  if (!empty($_POST["humaninput"])) {
+    $human = $_POST["humaninput"];
+  } else {
+    $notice2 = " Vali stuudio!";
+  }
+  if (!empty($_POST["positioninput"])) {
+    $position = $_POST["positioninput"];
+  } else {
+    $notice2 = " Vali Amet!";
+  }
+  if (!empty($film2) and !empty($human) and !empty($position)) {
+    $notice2 = newfilmandhuman($film2, $human, $position);
   }
 }
 ?>
@@ -30,12 +53,19 @@ if (isset($_POST["filmjoinsubmit"])) {
 <p>See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
 <hr>
 <h2>Määrame filmile Stuudio</h2>
-<hr>
 <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
   <?php echo readfilm($film); ?>
   <?php echo readstudio($studio); ?>
 
   <input type="submit" name="filmjoinsubmit" value="Salvesta"><span><?php echo $notice; ?></span>
+</form>
+<hr>
+<h2>Määrame filmile Isiku ja Positsiooni</h2>
+<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+  <?php echo readfilm($film2); ?>
+  <?php echo readhuman($human); ?>
+  <?php echo readposition($position); ?>
+  <input type="submit" name="filmpersonsubmit" value="Salvesta"><span><?php echo $notice2; ?></span>
 </form>
 <hr>
 <ul>
--- kodu6/fn_films.php
+++ kodu6/fn_films.php
@@ -90,7 +90,59 @@ function readstudio($studio)
     $conn->close();
     return $notice;
 }
-function newrelation($film, $stuudio)
+
+function readhuman($human)
+{
+    $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+    $stmt = $conn->prepare("SELECT person_id, first_name FROM person");
+    echo $conn->error;
+    $stmt->bind_result($idfromdb, $humanfromdb);
+    $stmt->execute();
+    $humans = "";
+    while ($stmt->fetch()) {
+        $humans .= '<option value="' . $idfromdb . '"';
+        if ($idfromdb == $human) {
+            $humans .= " selected";
+        }
+        $humans .= ">" . $humanfromdb . "</option> \n";
+    }
+    if (!empty($humans)) {
+        $notice = '<select name="humaninput" id="humaninput">' . "\n";
+        $notice .= '<option value="" selected disabled>Isik</option>' . "\n";
+        $notice .= $humans;
+        $notice .= "</select> \n";
+    }
+    $stmt->close();
+    $conn->close();
+    return $notice;
+}
+function readposition($position)
+{
+    $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+    $stmt = $conn->prepare("SELECT position_id, position_name FROM position");
+    echo $conn->error;
+    $stmt->bind_result($idfromdb, $positionfromdb);
+    $stmt->execute();
+    $positions = "";
+    while ($stmt->fetch()) {
+        $positions .= '<option value="' . $idfromdb . '"';
+        if ($idfromdb == $position) {
+            $positions .= " selected";
+        }
+        $positions .= ">" . $positionfromdb . "</option> \n";
+    }
+    if (!empty($positions)) {
+        $notice = '<select name="positioninput" id="positioninput">' . "\n";
+        $notice .= '<option value="" selected disabled>Amet</option>' . "\n";
+        $notice .= $positions;
+        $notice .= "</select> \n";
+    }
+    $stmt->close();
+    $conn->close();
+    return $notice;
+}
+
+function newfilmandstudio($film, $stuudio)
 {
     $notice = "";
     $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
@@ -106,6 +158,32 @@ function newrelation($film, $stuudio)
         $stmt = $conn->prepare("INSERT INTO movie_by_production_company (movie_id, production_company_id) VALUES(?,?)");
         echo $conn->error;
         $stmt->bind_param("ii", $film, $stuudio);
+        if ($stmt->execute()) {
+            $notice = "Success!";
+        } else {
+            $notice = "ERROR: " . $stmt->error;
+        }
+    }
+    $stmt->close();
+    $conn->close();
+    return $notice;
+}
+function newfilmandhuman($film, $human, $position)
+{
+    $notice = "";
+    $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+    $stmt = $conn->prepare("SELECT person_in_movie_id FROM person_in_movie WHERE person_id = ? AND movie_id = ? AND position_id = ?");
+    echo $conn->error;
+    $stmt->bind_param("iii", $human, $film, $position);
+    $stmt->bind_result($idfromdb);
+    $stmt->execute();
+    if ($stmt->fetch()) {
+        $notice = "Olemas";
+    } else {
+        $stmt->close();
+        $stmt = $conn->prepare("INSERT INTO person_in_movie (person_id, movie_id, position_id) VALUES(?,?,?)");
+        echo $conn->error;
+        $stmt->bind_param("iii", $human, $film, $position);
         if ($stmt->execute()) {
             $notice = "Success!";
         } else {
